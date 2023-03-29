e1c085c63f301c7740b0eefc9b8021a3b6ff8496
knowOne08 <yashdarji23082004@gmail.com>
2023-03-29 19:41:46 +0530

    Testing

--- App/navigator.js
+++ App/navigator.js
@@ -12,31 +12,34 @@ console.log(geoLongitude)
     document.addEventListener('DOMContentLoaded', function() {
         // console.log('here')
         let scene = document.getElementById('scene')
-        let entity = document.createElement('a-entity');
-        // Do something with myElement
-        // entity.setAttribute('gps-entity-place', `latitude: ${geoLatitude + 0.001} ; longitude: ${geoLongitude};`);
-        entity.setAttribute("gps-entity-place", `latitude: 23.057793; longitude: 72.663406;`); //M-block
-        entity.setAttribute('geometry', 'primitive: cone');
-        // entity.setAttribute('position', `0 0 1  `);
-        entity.setAttribute('material', 'color: red');
-        entity.setAttribute('scale', '15 15 15');
-        // entity.setAttribute('animation-mixer', '');
-        
-        // window.location.assign("./navigator.html?currentCoords=currentLocation&destinationCoords=destinationLocation");
-        console.log(entity)
-        console.log(scene)
+        // let entity = document.createElement('a-entity');
+        let entity = document.getElementById('entity');
 
-        // let destinationEntity = document.createElement('a-entity');
-        // destinationEntity.setAttribute('gps-entity-place', `latitude: ${geoLatitude + 0.001} ; longitude: ${geoLongitude};`);
-        // // destinationEntity.setAttribute('geometry', 'primitive: sphere');
-        // destinationEntity.setAttribute('position', `${geoLatitude + 0.001} 0 ${geoLongitude }`);
-        // destinationEntity.setAttribute('material', 'color: green');
-        // destinationEntity.setAttribute('scale', '20 20 20');
         
-        // console.log(destinationEntity)
-        // scene.appendChild(destinationEntity);
-        scene.appendChild(entity);
-        // alert(JSON.stringify('yash'))
+        // // Do something with myElement
+        // // entity.setAttribute('gps-entity-place', `latitude: ${geoLatitude + 0.001} ; longitude: ${geoLongitude};`);
+        // entity.setAttribute("gps-entity-place", `latitude: 23.057793; longitude: 72.663406;`); //M-block
+        // entity.setAttribute('geometry', 'primitive: cone');
+        // // entity.setAttribute('position', `0 0 1  `);
+        entity.setAttribute('material', 'color: red');
+        // entity.setAttribute('scale', '15 15 15');
+        // // entity.setAttribute('animation-mixer', '');
+        console.log(entity)
+        // // window.location.assign("./navigator.html?currentCoords=currentLocation&destinationCoords=destinationLocation");
+        // console.log(entity)
+        // console.log(scene)
+
+        // // let destinationEntity = document.createElement('a-entity');
+        // // destinationEntity.setAttribute('gps-entity-place', `latitude: ${geoLatitude + 0.001} ; longitude: ${geoLongitude};`);
+        // // // destinationEntity.setAttribute('geometry', 'primitive: sphere');
+        // // destinationEntity.setAttribute('position', `${geoLatitude + 0.001} 0 ${geoLongitude }`);
+        // // destinationEntity.setAttribute('material', 'color: green');
+        // // destinationEntity.setAttribute('scale', '20 20 20');
+        
+        // // console.log(destinationEntity)
+        // // scene.appendChild(destinationEntity);
+        // scene.appendChild(entity);
+        // // alert(JSON.stringify('yash'))
     });
 
 
