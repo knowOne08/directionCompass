c542743860978682fb4c7b713e864bda59838876
knowOne08 <yashdarji23082004@gmail.com>
2023-03-29 19:23:00 +0530

    Changed it to a cone

--- App/navigator.js
+++ App/navigator.js
@@ -16,7 +16,7 @@ console.log(geoLongitude)
         // Do something with myElement
         entity.setAttribute('gps-entity-place', `latitude: ${geoLatitude + 0.001} ; longitude: ${geoLongitude};`);
         // entity.setAttribute("gps-entity-place", `latitude: 23.057793; longitude: 72.663406;`); //M-block
-        entity.setAttribute('geometry', 'primitive: box');
+        entity.setAttribute('geometry', 'primitive: cone');
         // entity.setAttribute('position', `0 0 1  `);
         entity.setAttribute('material', 'color: red');
         entity.setAttribute('scale', '15 15 15');
