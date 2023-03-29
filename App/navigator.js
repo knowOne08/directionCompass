3ab7e74ad7a4fd503dd3dd863f046d205b3f223f
knowOne08 <yashdarji23082004@gmail.com>
2023-03-29 11:55:56 +0530

    Testing

--- App/navigator.js
+++ App/navigator.js
@@ -11,22 +11,22 @@ console.log(geoLongitude)
     // entity.setAttribute('gps-entity-place', `latitude: ${destinationLocation.location.lat}; longitude: ${destinationLocation.location.lng};`);
     document.addEventListener('DOMContentLoaded', function() {
         let scene = document.getElementById('scene')
-        let entity = document.createElement('a-entity');
-        // Do something with myElement
-        // entity.setAttribute('gps-entity-place', `latitude: ${latitude} ; longitude: ${longitude};`);
-        entity.setAttribute('gps-entity-place', `latitude: ${geoLatitude + 0.001} ; longitude: ${geoLongitude};`);
-        // entity.setAttribute("gps-entity-place", `latitude: 23.10796558905296; longitude: 72.59461530062512;`); //M-block
-        entity.setAttribute('geometry', 'primitive: box; height: 1; width: 1;');
-        entity.setAttribute('position', '0 0 1');
-        entity.setAttribute('material', 'color: red');
-        entity.setAttribute('scale', '20 20 20');
-        // entity.setAttribute('animation-mixer', '');
+        // let entity = document.createElement('a-entity');
+        // // Do something with myElement
+        // // entity.setAttribute('gps-entity-place', `latitude: ${latitude} ; longitude: ${longitude};`);
+        // entity.setAttribute('gps-entity-place', `latitude: ${geoLatitude + 0.001} ; longitude: ${geoLongitude};`);
+        // // entity.setAttribute("gps-entity-place", `latitude: 23.10796558905296; longitude: 72.59461530062512;`); //M-block
+        // entity.setAttribute('geometry', 'primitive: box; height: 1; width: 1;');
+        // entity.setAttribute('position', '0 0 1');
+        // entity.setAttribute('material', 'color: red');
+        // entity.setAttribute('scale', '20 20 20');
+        // // entity.setAttribute('animation-mixer', '');
         
         // window.location.assign("./navigator.html?currentCoords=currentLocation&destinationCoords=destinationLocation");
-        console.log(entity)
+        // console.log(entity)
         console.log(scene)
 
-        const destinationEntity = document.createElement('a-entity');
+        let destinationEntity = document.createElement('a-entity');
         destinationEntity.setAttribute('gps-entity-place', `latitude: ${latitude} ; longitude: ${longitude};`);
         destinationEntity.setAttribute('geometry', 'primitive: box; height: 1; width: 1;');
         destinationEntity.setAttribute('position', '0 0 1');
@@ -35,7 +35,7 @@ console.log(geoLongitude)
         
         console.log(destinationEntity)
         scene.appendChild(destinationEntity);
-        scene.appendChild(entity);
+        // scene.appendChild(entity);
         // alert(JSON.stringify('yash'))
     });
 
