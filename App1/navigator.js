5586e87c2e77c5d1f6bdafc6dbd074165a7e2223
knowOne08 <yashdarji23082004@gmail.com>
2023-03-31 12:15:31 +0530

    Trying to fix the the arrow

--- App1/navigator.js
+++ App1/navigator.js
@@ -239,13 +239,14 @@ window.onload = () => {
 
                     // Attempt of making arrow through cone                    
                     const arrowEntity = document.createElement('a-entity');
-                    arrowEntity.setAttribute('gps-entity-place', `latitude: ${position.coords.latitude + 0.0001}; longitude: ${position.coords.longitude};`);
+                    arrowEntity.setAttribute('gps-entity-place', `latitude: ${position.coords.latitude + 0.001}; longitude: ${position.coords.longitude};`);
                     arrowEntity.setAttribute('geometry', 'primitive: cone;');
                     arrowEntity.setAttribute('material', 'color: red;');
                     arrowEntity.setAttribute('scale', '10 10 30');
                     // arrowEntity.setAttribute('rotation', '0 0 50');
                     arrowEntity.setAttribute('position', '0 0 0');
                     arrowEntity.setAttribute('look-at', '[gps-camera]');
+                    arrowEntity.setAttribute('fixed','true')
                     scene.appendChild(arrowEntity);
                     
                     console.log(arrowEntity)
