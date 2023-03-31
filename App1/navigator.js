71a5c6a9721c2b1e3929f566444832cc3ceb23e4
knowOne08 <yashdarji23082004@gmail.com>
2023-03-31 14:55:16 +0530

    Testing arrow

--- App1/navigator.js
+++ App1/navigator.js
@@ -239,7 +239,7 @@ window.onload = () => {
 
                     // Attempt of making arrow through cone                    
                     const arrowEntity = document.createElement('a-entity');
-                    arrowEntity.setAttribute('gps-entity-place', `latitude: ${position.coords.latitude + 0.001}; longitude: ${position.coords.longitude};`);
+                    arrowEntity.setAttribute('gps-entity-place', `latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude + 0.001};`);
                     // arrowEntity.setAttribute('geometry', 'primitive: cone;');
 
                     // arrowEntity.setAttribute('material', 'color: red;');
