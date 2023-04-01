efd13f297aed47c5200a99948263beb06c5fcf41
knowOne08 <yashdarji23082004@gmail.com>
2023-04-01 10:11:08 +0530

    Testing

--- App1/navigator.js
+++ App1/navigator.js
@@ -187,7 +187,7 @@ window.onload = () => {
                     arrowEntity.setAttribute('gltf-model', './assets/arrow.gltf');
                     arrowEntity.setAttribute('scale', '2 2 2');
                     arrowEntity.setAttribute('rotation', '0 45 0');
-                    arrowEntity.setAttribute('position', '0 -0.9 3');
+                    arrowEntity.setAttribute('position', '0 -0.9 10');
                     arrowEntity.setAttribute('look-at', '[gps-camera]');
                     arrowEntity.setAttribute('fixed','true')
                     // arrowEntity.addEventListener('loaded', () => {
